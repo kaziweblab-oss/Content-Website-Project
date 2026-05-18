2ccf6b82dd06a3269deef698a60311941e4bc639
Kazi Tasin Hossen <kwlkali.org@gmail.com>
2026-05-18 10:22:02 +0000

    change section gap

--- src/Components/HomeComponent/SubscribeSection/index.tsx
+++ src/Components/HomeComponent/SubscribeSection/index.tsx
@@ -13,7 +13,7 @@ function SubscriptionSection() {
             <div className={style.subscribeSectionHeaderIcon}>
               <RiMailSendLine className={style.sendMailIcon} />
             </div>
-            <div /*className={style.subscribeSectionHeaderText}*/>
+            <div>
               <p>Subscribe to our Newsletter</p>
               <p>Stay tuned! Recive News and Discounts</p>
             </div>
